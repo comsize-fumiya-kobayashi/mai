7f9cc8a547b9f961c4e095417e9c9819e7151124
Comsize.Archna <archna@comsize.com>
2023-11-30 13:25:31 +0900

    編集したUpdateBean

--- src/main/java/model/entity/UpdateBean.java
+++ src/main/java/model/entity/UpdateBean.java
@@ -1,78 +1,121 @@
 package model.entity;
 
+import java.sql.Date;
+import java.sql.Timestamp;
+
 public class UpdateBean {
 	
 	private int taskId;
 	private String taskName;
 	private int categoryId;
 	private String categoryName;
-	private int limitDate;
+	private Date limitDate;
 	private String userId;
 	private String userName;
-	private int statusCode;
+	private String statusCode;
 	private String statusName;
 	private String memo;
+	private Timestamp createDatetime;
+	private Timestamp updateDatetime;
 	
+	public UpdateBean() {
+		super();
+	}
+
 	public int getTaskId() {
 		return taskId;
 	}
+
 	public void setTaskId(int taskId) {
 		this.taskId = taskId;
 	}
+
 	public String getTaskName() {
 		return taskName;
 	}
+
 	public void setTaskName(String taskName) {
 		this.taskName = taskName;
 	}
+
 	public int getCategoryId() {
 		return categoryId;
 	}
+
 	public void setCategoryId(int categoryId) {
 		this.categoryId = categoryId;
 	}
+
 	public String getCategoryName() {
 		return categoryName;
 	}
+
 	public void setCategoryName(String categoryName) {
 		this.categoryName = categoryName;
 	}
-	public int getLimitDate() {
+
+	public Date getLimitDate() {
 		return limitDate;
 	}
-	public void setLimitDate(int limitDate) {
+
+	public void setLimitDate(Date limitDate) {
 		this.limitDate = limitDate;
 	}
+
 	public String getUserId() {
 		return userId;
 	}
+
 	public void setUserId(String userId) {
 		this.userId = userId;
 	}
+
 	public String getUserName() {
 		return userName;
 	}
+
 	public void setUserName(String userName) {
 		this.userName = userName;
 	}
-	public int getStatusCode() {
+
+	public String getStatusCode() {
 		return statusCode;
 	}
-	public void setStatusCode(int statusCode) {
+
+	public void setStatusCode(String statusCode) {
 		this.statusCode = statusCode;
 	}
+
 	public String getStatusName() {
 		return statusName;
 	}
+
 	public void setStatusName(String statusName) {
 		this.statusName = statusName;
 	}
+
 	public String getMemo() {
 		return memo;
 	}
+
 	public void setMemo(String memo) {
 		this.memo = memo;
 	}
-	
+
+	public Timestamp getCreateDatetime() {
+		return createDatetime;
+	}
+
+	public void setCreateDatetime(Timestamp createDatetime) {
+		this.createDatetime = createDatetime;
+	}
+
+	public Timestamp getUpdateDatetime() {
+		return updateDatetime;
+	}
+
+	public void setUpdateDatetime(Timestamp updateDatetime) {
+		this.updateDatetime = updateDatetime;
+	}
 	
 }
