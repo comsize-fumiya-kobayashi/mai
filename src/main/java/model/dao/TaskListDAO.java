29986994d18d0f3301066b3a42810c132d80efb4
Comsize.Archna <archna@comsize.com>
2023-12-04 10:48:46 +0900

    タスク一覧のDAO修正

--- src/main/java/model/dao/TaskListDAO.java
+++ src/main/java/model/dao/TaskListDAO.java
@@ -17,7 +17,7 @@ public class TaskListDAO {
 			throws SQLException, ClassNotFoundException {
 
 		List<TaskListBean> taskList = new ArrayList<TaskListBean>();
-		String sql = "SELECT t1.task_name, t2.category_name, t1.limit_date, t3.user_name, t4.status_name, t1.memo "
+		String sql = "SELECT t1.task_id, t1.task_name, t2.category_name, t1.limit_date, t3.user_name, t4.status_name, t1.memo "
 				+ "FROM t_task t1 "
 				+ "LEFT JOIN m_category t2 ON t1.category_id = t2.category_id "
 				+ "LEFT JOIN m_user t3 ON t1.user_id = t3.user_id "
@@ -30,6 +30,7 @@ public class TaskListDAO {
 
 			// 結果の操作
 			while (res.next()) {
+				int taskId = res.getInt("task_id");
 				String taskName = res.getString("task_name");
 				String categoryName = res.getString("category_name");
 				Date limitDate = res.getDate("limit_date");
@@ -38,6 +39,7 @@ public class TaskListDAO {
 				String memo = res.getString("memo");
 				
 				TaskListBean task = new TaskListBean();
+				task.setTaskId(taskId);
 				task.setTaskName(taskName);
 				task.setCategoryName(categoryName);
 				task.setLimitDate(limitDate);
