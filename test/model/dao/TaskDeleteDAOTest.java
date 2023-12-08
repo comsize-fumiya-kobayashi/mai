6744967bc484381ab0c0aa87778064ff647210a1
Comsize.Archna <archna@comsize.com>
2023-12-08 14:36:23 +0900

    DELETEDAOのSELECT

--- test/model/dao/TaskDeleteDAOTest.java
+++ test/model/dao/TaskDeleteDAOTest.java
@@ -1,13 +1,17 @@
 package model.dao;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.*;
 
+import java.sql.Date;
 import java.sql.SQLException;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
-class TaskDeleteDAOTest {
+import model.entity.TaskCategoryUserStatusBean;
 
+class TaskDeleteDAOTest {
+	@Disabled
 	@Test
 	void test_deleteTask() {
 		TaskDeleteDAO dao = new TaskDeleteDAO();
@@ -21,5 +25,26 @@ class TaskDeleteDAOTest {
 		}
 		assertEquals(1,processingNumber);
 	}
-
+	
+	@Test
+	void test_selectTask() {
+		TaskDeleteDAO dao = new TaskDeleteDAO();
+		TaskCategoryUserStatusBean tName = null;
+		try {
+			tName = dao.selectTask(22);
+		} catch (ClassNotFoundException | SQLException e) {
+			// TODO 自動生成された catch ブロック
+			e.printStackTrace();
+		}
+		assertEquals(22,tName.getTaskId());
+		assertEquals("テストサンプル",tName.getTaskName());
+		assertEquals(1, tName.getCategoryId());
+		assertEquals("新商品A:開発プロジェクト",tName.getCategoryName());
+		assertEquals(Date.valueOf("2023-12-08"),tName.getLimitDate());
+		assertEquals("admin", tName.getUserId());
+		assertEquals("山田",tName.getUserName());
+		assertEquals("00",tName.getStatusCode());
+		assertEquals("未着手",tName.getStatusName());
+		assertEquals("メモ",tName.getMemo());
+	}
 }
