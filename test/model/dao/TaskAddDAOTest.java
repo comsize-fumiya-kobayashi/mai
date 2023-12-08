4432bd7423c2cd9d6f5710c6681bf7bb320b12d9
Comsize.Archna <archna@comsize.com>
2023-12-08 13:25:09 +0900

    TASKADDDAOのテスト

--- test/model/dao/TaskAddDAOTest.java
+++ test/model/dao/TaskAddDAOTest.java
@@ -1,11 +1,38 @@
 package model.dao;
 
+import static org.junit.Assert.*;
+
+import java.sql.Date;
+import java.sql.SQLException;
+
 import org.junit.jupiter.api.Test;
+
+import model.entity.TaskCategoryUserStatusBean;
 
 class TaskAddDAOTest {
 
 	@Test
-	void testInsertTask01() {
+	void test_insertTask() {
+		TaskAddDAO dao = new TaskAddDAO();
+		TaskCategoryUserStatusBean taskCategoryUserStatusBean = new TaskCategoryUserStatusBean();
 		
+		taskCategoryUserStatusBean.setTaskName("テストサンプル");
+		taskCategoryUserStatusBean.setCategoryId(1);
+		taskCategoryUserStatusBean.setLimitDate(Date.valueOf("2023-12-8"));
+		taskCategoryUserStatusBean.setUserId("admin");
+		taskCategoryUserStatusBean.setStatusCode("00");
+		taskCategoryUserStatusBean.setMemo("メモ");
+		
+	int processingNumber = 0;
+		
+		try {
+			processingNumber = dao.insertTask(taskCategoryUserStatusBean);
+		} catch (ClassNotFoundException | SQLException e) {
+			e.printStackTrace();
+		}
+        
+        // Check if the data was updated correctly
+        assertEquals(1,processingNumber);
 	}
+	
 }
