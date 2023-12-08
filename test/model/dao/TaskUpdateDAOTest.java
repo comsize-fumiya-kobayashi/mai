7cc932369d6d475e770a688d59f9d1b3ed9877b3
Comsize.Archna <archna@comsize.com>
2023-12-08 13:23:37 +0900

    UPDATEDAOのSELECTテスト

--- test/model/dao/TaskUpdateDAOTest.java
+++ test/model/dao/TaskUpdateDAOTest.java
@@ -5,12 +5,13 @@ import static org.junit.Assert.*;
 import java.sql.Date;
 import java.sql.SQLException;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import model.entity.TaskCategoryUserStatusBean;
 
 class TaskUpdateDAOTest {
-
+	@Disabled
 	@Test
 	void test_updateTask() {
 		TaskUpdateDAO dao = new TaskUpdateDAO();
@@ -22,19 +23,37 @@ class TaskUpdateDAOTest {
 		taskCategoryUserStatusBean.setUserId("admin");
 		taskCategoryUserStatusBean.setStatusCode("00");
 		taskCategoryUserStatusBean.setMemo("メモ");
-		
+
 		int processingNumber = 0;
-		
 		try {
 			processingNumber = dao.updateTask(taskCategoryUserStatusBean);
 		} catch (ClassNotFoundException | SQLException e) {
 			e.printStackTrace();
 		}
-		
-        
-        // Check if the data was updated correctly
-        assertEquals(1,processingNumber);
+		// Check if the data was updated correctly
+		assertEquals(1, processingNumber);
 	}
-		
+	
+	@Test
+	void test_selectTask() {
+		TaskUpdateDAO dao = new TaskUpdateDAO();
 
+		TaskCategoryUserStatusBean tname = null;
+		try {
+			tname = dao.selectTask(22);
+		} catch (ClassNotFoundException | SQLException e) {
+			e.printStackTrace();
+		}
+		assertEquals(22, tname.getTaskId());
+		assertEquals("テストサンプル",tname.getTaskName());
+		assertEquals(1, tname.getCategoryId());
+		assertEquals("新商品A:開発プロジェクト",tname.getCategoryName());
+		assertEquals(Date.valueOf("2023-12-08"),tname.getLimitDate());
+		assertEquals("admin", tname.getUserId());
+		assertEquals("山田",tname.getUserName());
+		assertEquals("00",tname.getStatusCode());
+		assertEquals("未着手",tname.getStatusName());
+		assertEquals("メモ",tname.getMemo());
+	}
+	
 }
